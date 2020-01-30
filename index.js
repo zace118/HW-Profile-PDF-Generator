e485ab96f7424e8884994358bb3ce958f50a8b37
Zac Eskridge <zace118@gmail.com>
2020-01-29 18:34:12 -0700

    Getting 404 error on my github call. Checkpoint

--- index.js
+++ index.js
@@ -2,45 +2,21 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const axios = require('axios');
 
-// let username;
+let username;
 
-// async function doStuff() {
-//     try {
-//         const { prompt } = await inquirer.prompt([
-//             {
-//                 type: "list",
-//                 message: "What's your favorite color?",
-//                 name: "favColor",
-//                 choices: [
-//                     'red',
-//                     'blue',
-//                     'green',
-//                     'yellow',
-//                     'pink',
-//                     'black'
-//                 ]
-//             },
-//             {
-//                 type: "input",
-//                 message: "What's your GitHub username?",
-//                 name: "username"
-//             }
-//         ]);
+function axiosCall() {
+    // axios call github api using username
+    axios
+        .get(`https://github.com/users/${username}`)
+        .then(function (res) {
+            console.log(res.data.login);
+            console.log(res.data.location);
 
-//         console.log(prompt);
-
-//         // axios call github api using username
-//         // const { data } = await axios.get(
-//         //     `https://api.github.com/search/users?q=${username}`
-//         // );
-//         // console.log(data);
-//     } catch (err) {
-//         console.log(err);
-//     }
-// };
-
-// doStuff();
-
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
 
 inquirer
     .prompt([
@@ -65,14 +41,10 @@ inquirer
     ])
     .then(function (res) {
         console.log(res);
-        const username = res.username;
-        // axios call github api using username
-        axios
-            .get(`https://api.github.com/search/users?q=${username}`)
-            .then(function (res) {
-                console.log(res);
-            })
+        username = res.username;
+        axiosCall();
     });
+
 
 
     // converting html to pdf npm?
