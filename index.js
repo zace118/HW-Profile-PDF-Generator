602941b731a7defbb09b2326a691e69d5706ce36
Zac Eskridge <zace118@gmail.com>
2020-01-29 17:19:37 -0700

    Oh butts....I think I did something wrong.y quite yet. Still striving!

--- index.js
+++ index.js
@@ -1,13 +1,79 @@
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
+const axios = require('axios');
 
-];
+// let username;
 
-function writeToFile(fileName, data) {
+// async function doStuff() {
+//     try {
+//         const { prompt } = await inquirer.prompt([
+//             {
+//                 type: "list",
+//                 message: "What's your favorite color?",
+//                 name: "favColor",
+//                 choices: [
+//                     'red',
+//                     'blue',
+//                     'green',
+//                     'yellow',
+//                     'pink',
+//                     'black'
+//                 ]
+//             },
+//             {
+//                 type: "input",
+//                 message: "What's your GitHub username?",
+//                 name: "username"
+//             }
+//         ]);
 
-}
+//         console.log(prompt);
 
-function init() {
+//         // axios call github api using username
+//         // const { data } = await axios.get(
+//         //     `https://api.github.com/search/users?q=${username}`
+//         // );
+//         // console.log(data);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// };
 
-}
+// doStuff();
 
-init();
+
+inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "What's your favorite color?",
+            name: "favColor",
+            choices: [
+                'red',
+                'blue',
+                'green',
+                'yellow',
+                'pink',
+                'black'
+            ]
+        },
+        {
+            type: "input",
+            message: "What's your GitHub username?",
+            name: "username"
+        }
+    ])
+    .then(function (res) {
+        console.log(res);
+        const username = res.username;
+        // axios call github api using username
+        axios
+            .get(`https://api.github.com/search/users?q=${username}`)
+            .then(function (res) {
+                console.log(res);
+            })
+    });
+
+
+    // converting html to pdf npm?
+
